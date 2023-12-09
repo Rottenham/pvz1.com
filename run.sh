c43b0dd5e7ad111cb3af03c2b85fc2fe1718259e
OTZzzz <forum@crescb.com>
2023-12-09 16:53:20 +0800

    update

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for nm in */ ; do
+    if [ -d "$nm" ]; then
+        echo "Processing $nm folder..."
+        cd "$nm" && mdbook build && {
+            if [ $nm = "welcome/" ]; then
+                cp -r ./* /www/wwwroot/www.crescb.com/
+            else
+                cp -r ./* /www/wwwroot/www.crescb.com/"$nm"
+            fi
+        } && rm -rf "$nm" && cd ..
+        echo "Processing $nm folder completed."
+    fi
+done
