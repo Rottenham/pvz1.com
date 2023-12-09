96779a53ae903edf6ffb5a353aeda9d58794f75b
Rottenham <54915258+Rottenham@users.noreply.github.com>
2023-12-09 19:09:15 +0800

    User better variable naming in run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-git pull && for nm in */ ; do
-    if [ -d "$nm" ]; then
-        echo "Processing $nm folder..."
-        cd "$nm" && mdbook build && {
-            if [ "$nm" = "welcome/" ]; then
-                cp -r ./$nm/* /www/wwwroot/www.crescb.com/
+git pull && for folder_name in */ ; do
+    if [ -d "$folder_name" ]; then
+        echo "Processing $folder_name folder..."
+        cd "$folder_name" && mdbook build && {
+            if [ "$folder_name" = "welcome/" ]; then
+                cp -r ./$folder_name/* /www/wwwroot/www.crescb.com/
             else
-                cp -r ./$nm/* /www/wwwroot/www.crescb.com/"$nm"
+                cp -r ./$folder_name/* /www/wwwroot/www.crescb.com/"$folder_name"
             fi
         } && rm -rf "$folder_name" && cd ..
     fi
